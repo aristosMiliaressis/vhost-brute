82f607569e56105685580d37cc207dd07f1e53c2
unknown <aristosmiliaresis@gmail.com>
2024-01-05 18:31:04 +0200

    added response headers to html report

--- generate-report.sh
+++ generate-report.sh
@@ -1,20 +1,31 @@
 #!/bin/bash
+# Dependencies: gowitness, httpx, parallel, jq
 
 dnsfile=`mktemp`
 vhostfile=`mktemp`
 trap "rm $dnsfile $vhostfile" EXIT
 
-grep -hr '{"' | jq -r .Hostname | httpx > $dnsfile
+cat * | grep '^{"' | jq -r .Hostname | httpx > $dnsfile
 gowitness file -f $dnsfile --disable-db -F -P ./dns_screenshots
+cat $dnsfile | parallel -j 5 "curl -s -I -k {} > dns_screenshots/\$(echo {} | sed 's,://,-,').headers"
 
-cp /etc/hosts /etc/hosts.bak
-trap "mv /etc/hosts.bak /etc/hosts" EXIT
-grep -hr '{"' | jq -r '. | "\(.Address) \(.Hostname)"' | sed -E 's,https?://,,' | sed -E 's,:[0-9]+ , ,' >> /etc/hosts
+cp /etc/hosts ./hosts.bak
+trap "mv ./hosts.bak /etc/hosts" EXIT
+cat * | grep '^{"' | jq -r '. | "\(.Address) \(.Hostname)"' | sed -E 's,https?://,,' | sed -E 's,:[0-9]+ , ,' >> /etc/hosts
 
-grep -hr '{"' | jq -r .Hostname | httpx > $vhostfile
+cat * | grep '^{"' | jq -r .Hostname | httpx > $vhostfile
 gowitness file -f $vhostfile --disable-db -F -P ./vhost_screenshots
+cat $vhostfile | parallel -j 5 "curl -s -I -k {} > vhost_screenshots/\$(echo {} | sed 's,://,-,').headers"
 
-tablehtml=$(ls vhost_screenshots | while read file; do echo "<tr><td><b>$file</b></td><td><img src='dns_screenshots/$file'/></td><td><img src='vhost_screenshots/$file'/></td></tr>"; done)
+generate_report_row() {
+	dnsHeaders=$(cat dns_screenshots/$(echo $1 | sed 's/.png$/.headers/'))
+	vhostHeaders=$(cat vhost_screenshots/$(echo $1 | sed 's/.png$/.headers/'))
+	headerColumn="<b>DNS Headers</b><pre>$dnsHeaders</pre><b>VHOST Headers</b><pre>$vhostHeaders</pre>"
+	
+	echo "<tr><td>$headerColumn</td><td><img alt='N/A' src='dns_screenshots/$1'/></td><td><img src='vhost_screenshots/$1'/></td></tr>"
+}
+
+tablehtml=$(ls vhost_screenshots/*.png | sed 's,vhost_screenshots/,,' | while read file; do generate_report_row $file; done)
 
 cat >report.html <<EOF
 <!DOCTYPE html>
@@ -23,7 +34,12 @@ cat >report.html <<EOF
 		<style>
 			table, td {
 				border: 1px solid black;
+				background-color: #d5f4e6;
+				table-layout:fixed;
+				width: 100%;
 			}
+			img { width: 100%; }
+			pre, b { font-size: 40px; white-space: pre-wrap; }
 		</style>
 	</head>
 	<body>
