1b66e87249b47e4178749626a2bb615f91379afd
unknown <aristosmiliaresis@gmail.com>
2024-01-11 15:14:23 +0200

    report improvement

--- generate-report.sh
+++ generate-report.sh
@@ -10,7 +10,7 @@ gowitness file -f $dnsfile --disable-db -F -P ./dns_screenshots
 cat $dnsfile | parallel -j 5 "curl -s -I -k {} > dns_screenshots/\$(echo {} | sed 's,://,-,').headers"
 
 cp /etc/hosts ./hosts.bak
-trap "mv ./hosts.bak /etc/hosts" EXIT
+trap "cat ./hosts.bak > /etc/hosts" EXIT
 cat * | grep '^{"' | jq -r '. | "\(.Address) \(.Hostname)"' | sed -E 's,https?://,,' | sed -E 's,:[0-9]+ , ,' >> /etc/hosts
 
 cat * | grep '^{"' | jq -r .Hostname | httpx > $vhostfile
@@ -22,8 +22,9 @@ generate_report_row() {
 	vhostHeaders=$(cat vhost_screenshots/$(echo $1 | sed 's/.png$/.headers/'))
 	hostname=$(echo $1 | sed 's/.png$//' | sed -E 's/https?-//')
 	address=$(cat * | grep '^{\"' | jq -r "select(.Hostname == \"$hostname\") | .Address" | head -n 1)
+	bypass=$(cat * | grep '^{\"' | jq -r "select(.Hostname == \"$hostname\") | .WafBypass" | head -n 1)
 	header="<b>$hostname at $address</b><br/><br/>"
-	headerColumn="$header<b>DNS Headers</b><pre>$dnsHeaders</pre><b>VHOST Headers</b><pre>$vhostHeaders</pre>"
+	headerColumn="$header<b>DNS Headers</b><pre>$dnsHeaders</pre><b>VHOST Headers</b><pre>$vhostHeaders</pre>$bypass"
 	
 	echo "<tr><td>$headerColumn</td><td><img alt='N/A' src='dns_screenshots/$1'/></td><td><img src='vhost_screenshots/$1'/></td></tr>"
 }
@@ -42,7 +43,7 @@ cat >report.html <<EOF
 				width: 100%;
 			}
 			img { width: 100%; }
-			pre, b { font-size: 40px; white-space: pre-wrap; }
+			pre, b { font-size: 16px; white-space: pre-wrap; }
 		</style>
 	</head>
 	<body>
